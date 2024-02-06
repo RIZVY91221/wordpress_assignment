0d343fe6b515a73e0a13ef16d548b7137853d729
rizvy_ahmed <rizvy@spaceship.com.sg>
2024-02-06 17:00:45 +0600

    ST-16: Create modal components

---
+++ lib/core/widget/global/modal/app_modal.dart
@@ -0,0 +1,196 @@
+import 'package:app_base_flutter/core/theme/colors.dart';
+import 'package:app_base_flutter/core/theme/text.dart';
+import 'package:app_base_flutter/core/values/values.dart';
+import 'package:app_base_flutter/core/widget/global/button/wid_button_static.dart';
+import 'package:app_base_flutter/core/widget/global/modal/base_modal.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+class AlertModal extends BaseModal {
+  static void show({
+    Function()? onPressed,
+    String header = "Confirm cancel?",
+    String content =
+        "Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing industries for previewing layouts and visual mockups ",
+    Widget? body,
+    Widget? footer,
+    String button1 = "Ok",
+  }) =>
+      BaseModal.show(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            AppText.headline5(header),
+            AppGap.vertical20,
+            body ??
+                AppText.bodySmall(
+                  content,
+                  color: AppColor.darkLightest6C7576,
+                ),
+            AppGap.vertical20,
+            const Spacer(),
+            footer ??
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    const SizedBox.shrink(),
+                    button1.isNotEmpty
+                        ? AppStaticButton(
+                            onPressed: onPressed ?? () => Get.back(),
+                            backgroundColor: AppColor.primaryOne4B9EFF,
+                            padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 12),
+                            borderRadius: BorderRadius.circular(
+                              AppBorderRadius.br10,
+                            ),
+                            child: AppText.headline5(
+                              button1,
+                              color: AppColor.whiteFFFFFF,
+                            ),
+                          )
+                        : const SizedBox.shrink()
+                  ],
+                )
+          ],
+        ),
+        context: Get.context!,
+      );
+}
+
+class ConfirmationModal extends BaseModal {
+  static void show({
+    VoidCallback? onPressed,
+    String header = "Confirm cancel?",
+    Widget? widContent,
+    String content =
+        "Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing industries for previewing layouts and visual mockups ",
+    String button1 = "Confirm",
+    String button2 = "Cancel",
+  }) {
+    BaseModal.show(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          AppText.headline5(header),
+          AppGap.vertical20,
+          widContent ??
+              AppText.bodySmall(
+                content,
+                color: AppColor.darkLightest6C7576,
+              ),
+          AppGap.vertical20,
+          const Spacer(),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              button2.isNotEmpty
+                  ? AppStaticButton(
+                      onPressed: () => Get.back(),
+                      child: AppText.headline5(
+                        button2,
+                        color: AppColor.primaryOne4B9EFF,
+                      ),
+                    )
+                  : Container(),
+              AppGap.horizontal36,
+              button1.isNotEmpty
+                  ? AppStaticButton(
+                      onPressed: onPressed,
+                      backgroundColor: AppColor.primaryOne4B9EFF,
+                      padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 12),
+                      borderRadius: BorderRadius.circular(
+                        AppBorderRadius.br10,
+                      ),
+                      child: AppText.headline5(
+                        button1,
+                        color: AppColor.whiteFFFFFF,
+                      ),
+                    )
+                  : const SizedBox.shrink()
+            ],
+          )
+        ],
+      ),
+      context: Get.context!,
+    );
+  }
+}
+
+class DialogModal extends BaseModal {
+  static void show({
+    Function()? onPressed,
+    String header = "Dialog Header",
+    Widget? body,
+    String content =
+        "Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing industries for previewing layouts and visual mockups ",
+    Widget? footer,
+    String button1 = "Ok",
+    String button2 = "Cancel",
+  }) =>
+      BaseModal.show(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            AppText.headline5(header),
+            AppGap.vertical20,
+            body ??
+                AppText.bodySmall(
+                  content,
+                  color: AppColor.darkLightest6C7576,
+                ),
+            AppGap.vertical20,
+            const Spacer(),
+            footer ??
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    button2.isNotEmpty
+                        ? AppStaticButton(
+                            onPressed: () => Get.back(),
+                            child: AppText.headline5(
+                              button2,
+                              color: AppColor.primaryOne4B9EFF,
+                            ),
+                          )
+                        : const SizedBox.shrink(),
+                    AppGap.horizontal36,
+                    button1.isNotEmpty
+                        ? AppStaticButton(
+                            onPressed: onPressed,
+                            backgroundColor: AppColor.primaryOne4B9EFF,
+                            padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 12),
+                            borderRadius: BorderRadius.circular(
+                              AppBorderRadius.br10,
+                            ),
+                            child: AppText.headline5(
+                              button1,
+                              color: AppColor.whiteFFFFFF,
+                            ),
+                          )
+                        : const SizedBox.shrink()
+                  ],
+                )
+          ],
+        ),
+        context: Get.context!,
+      );
+}
+
+class FullModal extends BaseModal {
+  static void show({
+    AppBar? header,
+    List<Widget>? body,
+    String? headerTitle,
+    Icon? backIcon,
+    bool? titlePositionCenter,
+    bool isBackIconLeft = true,
+    VoidCallback? onPressBack,
+    VoidCallback? onPressConfirm,
+    List<Widget>? action,
+    Widget? footer,
+    double? buttonSize,
+    String? buttonText,
+    bool isFooterEnable = true,
+  }) {
+    Get.to(const BaseFullModal(), fullscreenDialog: true);
+  }
+}
---
+++ lib/core/widget/global/modal/base_modal.dart
@@ -0,0 +1,165 @@
+import 'dart:ui';
+
+import 'package:app_base_flutter/core/enum.dart';
+import 'package:app_base_flutter/core/theme/colors.dart';
+import 'package:app_base_flutter/core/widget/global/button/wid_button_elevated.dart';
+import 'package:app_base_flutter/core/widget/global/divider/wid_divider.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+class BaseModal {
+  static Future<void> show({
+    required BuildContext context,
+    double height = 285,
+    double borderRadius = 24,
+    double elevation = 10,
+    Widget? child,
+    EdgeInsets? insetPadding,
+    EdgeInsets? padding,
+    Color? backgroundColor,
+    Color? shadowColor,
+    bool barrierDismissible = false,
+    bool backgroundBlur = false,
+  }) {
+    return showCupertinoDialog(
+      context: context,
+      barrierDismissible: barrierDismissible,
+      barrierLabel: MaterialLocalizations.of(context).modalBarrierDismissLabel,
+      builder: (BuildContext context) {
+        return Dialog(
+          insetPadding: insetPadding ?? const EdgeInsets.symmetric(horizontal: 10),
+          shadowColor: shadowColor ?? Colors.white,
+          surfaceTintColor: backgroundColor,
+          backgroundColor: backgroundColor ?? Colors.white,
+          elevation: elevation,
+          shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(borderRadius)),
+          child: BackdropFilter(
+            filter: ImageFilter.blur(
+              sigmaX: backgroundBlur ? 15 : 0,
+              sigmaY: backgroundBlur ? 15 : 0,
+            ),
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(borderRadius),
+              child: Container(
+                color: backgroundColor ?? Colors.white,
+                height: height,
+                width: double.infinity,
+                padding: padding ?? const EdgeInsets.fromLTRB(24.0, 24.0, 24.0, 36.0),
+                child: child,
+              ),
+            ),
+          ),
+        );
+      },
+    );
+  }
+}
+
+class BaseFullModal extends StatelessWidget {
+  const BaseFullModal(
+      {Key? key,
+      this.headerTitle,
+      this.backIcon,
+      this.titlePositionCenter,
+      this.onPressBack,
+      this.isBackIconLeft = true,
+      this.action,
+      this.header,
+      this.body,
+      this.onPressConfirm,
+      this.footer,
+      this.buttonSize,
+      this.buttonText,
+      this.isFooterEnable = true})
+      : super(key: key);
+  final AppBar? header;
+  final List<Widget>? body;
+  final String? headerTitle;
+  final Icon? backIcon;
+  final bool? titlePositionCenter;
+  final bool isBackIconLeft;
+  final VoidCallback? onPressBack;
+  final VoidCallback? onPressConfirm;
+  final List<Widget>? action;
+  final Widget? footer;
+  final double? buttonSize;
+  final String? buttonText;
+  final bool isFooterEnable;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: AppColor.whiteFFFFFF,
+      appBar: header ?? headerWidget(),
+      body: SingleChildScrollView(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisSize: MainAxisSize.max,
+          children: [const AppDivider(), ...body ?? []],
+        ),
+      ),
+      bottomNavigationBar: footerWidget(),
+    );
+  }
+
+  AppBar headerWidget() {
+    return AppBar(
+      backgroundColor: AppColor.whiteFFFFFF,
+      centerTitle: titlePositionCenter ?? false,
+      elevation: 0,
+      title: Text(
+        headerTitle ?? "Modal Title",
+        style: Theme.of(Get.context!).textTheme.headlineSmall?.copyWith(color: AppColor.dark202125),
+      ),
+      leading: isBackIconLeft
+          ? IconButton(
+              icon: backIcon ??
+                  Icon(
+                    Icons.arrow_back_ios_outlined,
+                    color: AppColor.dark202125,
+                  ),
+              onPressed: () => onPressBack ?? Get.back(),
+            )
+          : const SizedBox.shrink(),
+      actions: action ??
+          [
+            !isBackIconLeft
+                ? IconButton(
+                    icon: Icon(
+                      Icons.arrow_back_ios_outlined,
+                      color: AppColor.dark202125,
+                    ),
+                    onPressed: () => onPressBack ?? Get.back(),
+                  )
+                : const SizedBox.shrink()
+          ],
+    );
+  }
+
+  Container footerWidget() {
+    return Container(
+      decoration: BoxDecoration(
+        color: Colors.white,
+        border: Border(top: BorderSide(width: 0.5, color: AppColor.disabledE4E5E7)),
+      ),
+      height: 100,
+      child: Padding(
+          padding: const EdgeInsets.only(left: 20, right: 20, bottom: 20),
+          child: footer ??
+              Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  AppElevatedButton(
+                    disabled: onPressConfirm != null ? false : true,
+                    onPressed: onPressConfirm,
+                    width: buttonSize ?? 120,
+                    text: buttonText ?? "Confirm",
+                    buttonType: ButtonType.secondary,
+                    buttonRound: ButtonRound.quarterRounded,
+                  ),
+                ],
+              )),
+    );
+  }
+}
