RIZVY91221/simple_chat_app

ST-19: Create radio input components

lib/core/widget/global/input_field/app_input.dart
@@ -1,5 +1,8 @@
+// ignore_for_file: must_be_immutable
+
 import 'package:app_base_flutter/core/values/values.dart';
 import 'package:app_base_flutter/core/widget/global/input_field/wid_input_component.dart';
+import 'package:app_base_flutter/core/widget/global/input_field/wid_input_custom_radio.dart';
 import 'package:app_base_flutter/core/widget/global/input_field/wid_text_field.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
@@ -365,4 +368,55 @@ class BaseUploadInputField extends WidgetInputComponent {
             onTap: onTap,
             focusNode: focusNode,
             onChange: onChange);
+}
+
+class BaseRadioInput extends CustomItems {
+  BaseRadioInput({
+    Key? key,
+    String? title = 'Radio Button',
+    required String name,
+    required String value,
+    bool isIconRequired = false,
+    double? boxSize,
+    Color? disableColor,
+    Function(String)? onChangeItem,
+    Widget Function(bool)? titleChild,
+  }) : super(
+          key: key,
+          title: title,
+          name: name,
+          value: value,
+          isIconRequired: isIconRequired,
+          boxSize: boxSize,
+          isCheckbox: false,
+          disableColor: disableColor,
+          onChangeItem: onChangeItem,
+          titleChild: titleChild,
+        );
+}
+
+class BaseRadioGroupInput extends WidgetCustomGroupInput {
+  BaseRadioGroupInput({
+    Key? key,
+    required List<String> typeList,
+    Function(String)? onChangeValue,
+    required String initialValue,
+    bool isIconRequired = false,
+    double? boxSize,
+    Color? disableColor,
+    bool isHorizontal = false,
+    Widget Function(bool)? titleChild,
+    List<String>? disable,
+  }) : super(
+            key: key,
+            initialValue: initialValue,
+            typeList: typeList,
+            onChangeValue: onChangeValue,
+            isIconRequired: isIconRequired,
+            boxSize: boxSize,
+            disableColor: disableColor,
+            isCheckbox: false,
+            isHorizontal: isHorizontal,
+            titleChild: titleChild,
+            disable: disable);
 }

lib/core/widget/global/input_field/wid_input_custom_radio.dart
@@ -1,0 +1,163 @@
+import 'package:app_base_flutter/core/theme/colors.dart';
+import 'package:app_base_flutter/core/theme/text.dart';
+import 'package:app_base_flutter/core/values/values.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+// ignore: must_be_immutable
+class WidgetCustomGroupInput extends StatelessWidget {
+  WidgetCustomGroupInput({
+    Key? key,
+    required this.typeList,
+    required this.initialValue,
+    this.onChangeValue,
+    this.boxSize,
+    this.isIconRequired = false,
+    this.disableColor,
+    this.isHorizontal = false,
+    this.isCheckbox = false,
+    this.titleChild,
+    this.disable,
+  }) : super(key: key);
+  final List<String> typeList;
+  final Function(String)? onChangeValue;
+  final String initialValue;
+  final bool isIconRequired;
+  final double? boxSize;
+  final Color? disableColor;
+  final bool isHorizontal;
+  final bool isCheckbox;
+  final Widget Function(bool)? titleChild;
+  final List<String>? disable;
+
+  Rx<String> value = ''.obs;
+  Rx<bool> horizontalAlignment = false.obs;
+  @override
+  Widget build(BuildContext context) {
+    value.value = initialValue;
+    horizontalAlignment.value = isHorizontal;
+    List<Widget> items = typeList
+        .map((e) => Obx(() => CustomItems(
+              title: e,
+              name: e,
+              boxSize: boxSize,
+              isIconRequired: isIconRequired,
+              isCheckbox: isCheckbox,
+              value: value.value,
+              disableColor: disableColor,
+              onChangeItem: (s) {
+                if (disable != null && disable!.any((e) => e.contains(s))) {
+                } else {
+                  value.value = s;
+                  onChangeValue?.call(s);
+                }
+              },
+              titleChild: titleChild,
+            )))
+        .toList();
+    return Obx(
+      () => !horizontalAlignment.value
+          ? Column(
+              children: items,
+            )
+          : Row(
+              children: items,
+            ),
+    );
+  }
+}
+
+// ignore: must_be_immutable
+class CustomItems extends StatelessWidget {
+  CustomItems({
+    Key? key,
+    this.title = 'Radio Button',
+    required this.name,
+    required this.value,
+    this.onChangeItem,
+    this.isCheckbox = false,
+    this.boxSize,
+    this.isIconRequired = true,
+    this.disableColor,
+    this.titleChild,
+  }) : super(key: key);
+
+  final String? title;
+  final String name;
+  final String value;
+  final bool isIconRequired;
+  final bool isCheckbox;
+  final double? boxSize;
+  final Color? disableColor;
+  final Function(String)? onChangeItem;
+  final Widget Function(bool)? titleChild;
+
+  Rx<bool> circleClicked = false.obs;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(left: AppPadding.p20, bottom: AppPadding.p12),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: [
+          ClipRRect(
+            borderRadius: BorderRadius.circular(isCheckbox ? 0 : 27),
+            child: InkWell(
+              splashColor: Colors.transparent,
+              highlightColor: Colors.transparent,
+              onTap: () async {
+                circleClicked.value = !circleClicked.value;
+                onChangeItem?.call(name);
+                // checkItem.value = orderType;
+              },
+              child: Container(
+                margin: const EdgeInsets.only(top: 0),
+                width: boxSize ?? 20,
+                height: boxSize ?? 20,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(isCheckbox ? 0 : 27),
+                  border: Border.all(
+                    width: boxSize != null ? boxSize! / 15 : 1,
+                    color: value == name ? AppColor.primaryOne4B9EFF : disableColor ?? AppColor.inputBorderRegularE4E5E7,
+                  ),
+                  //color: circleClicked.value ? AppColor.secondaryFour877DF8 : AppColor.transparent,
+                ),
+                child: value == name
+                    ? Padding(
+                        padding: EdgeInsets.all(isIconRequired
+                            ? 0.0
+                            : boxSize != null
+                                ? boxSize! * 0.22
+                                : 4),
+                        child: Container(
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(isCheckbox ? 0 : 27),
+                            color: AppColor.primaryOne4B9EFF,
+                          ),
+                          child: isIconRequired
+                              ? Center(
+                                  child: Icon(
+                                    Icons.done,
+                                    size: boxSize != null ? boxSize! * 0.80 : 17,
+                                    color: AppColor.whiteFFFFFF,
+                                  ),
+                                )
+                              : const SizedBox.shrink(),
+                        ),
+                      )
+                    : Container(),
+              ),
+            ),
+          ),
+          AppGap.horizontal10,
+          titleChild?.call(value == name) ??
+              AppText.headline6(
+                title,
+                color: value == name ? AppColor.darkLight4D4D50 : disableColor ?? AppColor.inputBorderRegularE4E5E7,
+              )
+        ],
+      ),
+    );
+  }
+}

RIZVY91221/wordpress_assignment

lib/core/widget/global/input_field/app_input.dart
@@ -1,5 +1,8 @@
+// ignore_for_file: must_be_immutable
+
 import 'package:bs_assignment/core/values/values.dart';
 import 'package:bs_assignment/core/widget/global/input_field/wid_input_component.dart';
+import 'package:bs_assignment/core/widget/global/input_field/wid_input_custom_radio.dart';
 import 'package:bs_assignment/core/widget/global/input_field/wid_text_field.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
@@ -361,4 +364,55 @@ class BaseUploadInputField extends WidgetInputComponent {
             onTap: onTap,
             focusNode: focusNode,
             onChange: onChange);
+}
+
+class BaseRadioInput extends CustomItems {
+  BaseRadioInput({
+    Key? key,
+    String? title = 'Radio Button',
+    required String name,
+    required String value,
+    bool isIconRequired = false,
+    double? boxSize,
+    Color? disableColor,
+    Function(String)? onChangeItem,
+    Widget Function(bool)? titleChild,
+  }) : super(
+          key: key,
+          title: title,
+          name: name,
+          value: value,
+          isIconRequired: isIconRequired,
+          boxSize: boxSize,
+          isCheckbox: false,
+          disableColor: disableColor,
+          onChangeItem: onChangeItem,
+          titleChild: titleChild,
+        );
+}
+
+class BaseRadioGroupInput extends WidgetCustomGroupInput {
+  BaseRadioGroupInput({
+    Key? key,
+    required List<String> typeList,
+    Function(String)? onChangeValue,
+    required String initialValue,
+    bool isIconRequired = false,
+    double? boxSize,
+    Color? disableColor,
+    bool isHorizontal = false,
+    Widget Function(bool)? titleChild,
+    List<String>? disable,
+  }) : super(
+            key: key,
+            initialValue: initialValue,
+            typeList: typeList,
+            onChangeValue: onChangeValue,
+            isIconRequired: isIconRequired,
+            boxSize: boxSize,
+            disableColor: disableColor,
+            isCheckbox: false,
+            isHorizontal: isHorizontal,
+            titleChild: titleChild,
+            disable: disable);
 }

lib/core/widget/global/input_field/wid_input_custom_radio.dart
@@ -1,0 +1,163 @@
+import 'package:bs_assignment/core/theme/colors.dart';
+import 'package:bs_assignment/core/theme/text.dart';
+import 'package:bs_assignment/core/values/values.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+// ignore: must_be_immutable
+class WidgetCustomGroupInput extends StatelessWidget {
+  WidgetCustomGroupInput({
+    Key? key,
+    required this.typeList,
+    required this.initialValue,
+    this.onChangeValue,
+    this.boxSize,
+    this.isIconRequired = false,
+    this.disableColor,
+    this.isHorizontal = false,
+    this.isCheckbox = false,
+    this.titleChild,
+    this.disable,
+  }) : super(key: key);
+  final List<String> typeList;
+  final Function(String)? onChangeValue;
+  final String initialValue;
+  final bool isIconRequired;
+  final double? boxSize;
+  final Color? disableColor;
+  final bool isHorizontal;
+  final bool isCheckbox;
+  final Widget Function(bool)? titleChild;
+  final List<String>? disable;
+
+  Rx<String> value = ''.obs;
+  Rx<bool> horizontalAlignment = false.obs;
+  @override
+  Widget build(BuildContext context) {
+    value.value = initialValue;
+    horizontalAlignment.value = isHorizontal;
+    List<Widget> items = typeList
+        .map((e) => Obx(() => CustomItems(
+              title: e,
+              name: e,
+              boxSize: boxSize,
+              isIconRequired: isIconRequired,
+              isCheckbox: isCheckbox,
+              value: value.value,
+              disableColor: disableColor,
+              onChangeItem: (s) {
+                if (disable != null && disable!.any((e) => e.contains(s))) {
+                } else {
+                  value.value = s;
+                  onChangeValue?.call(s);
+                }
+              },
+              titleChild: titleChild,
+            )))
+        .toList();
+    return Obx(
+      () => !horizontalAlignment.value
+          ? Column(
+              children: items,
+            )
+          : Row(
+              children: items,
+            ),
+    );
+  }
+}
+
+// ignore: must_be_immutable
+class CustomItems extends StatelessWidget {
+  CustomItems({
+    Key? key,
+    this.title = 'Radio Button',
+    required this.name,
+    required this.value,
+    this.onChangeItem,
+    this.isCheckbox = false,
+    this.boxSize,
+    this.isIconRequired = true,
+    this.disableColor,
+    this.titleChild,
+  }) : super(key: key);
+
+  final String? title;
+  final String name;
+  final String value;
+  final bool isIconRequired;
+  final bool isCheckbox;
+  final double? boxSize;
+  final Color? disableColor;
+  final Function(String)? onChangeItem;
+  final Widget Function(bool)? titleChild;
+
+  Rx<bool> circleClicked = false.obs;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(left: AppPadding.p20, bottom: AppPadding.p12),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: [
+          ClipRRect(
+            borderRadius: BorderRadius.circular(isCheckbox ? 0 : 27),
+            child: InkWell(
+              splashColor: Colors.transparent,
+              highlightColor: Colors.transparent,
+              onTap: () async {
+                circleClicked.value = !circleClicked.value;
+                onChangeItem?.call(name);
+                // checkItem.value = orderType;
+              },
+              child: Container(
+                margin: const EdgeInsets.only(top: 0),
+                width: boxSize ?? 20,
+                height: boxSize ?? 20,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(isCheckbox ? 0 : 27),
+                  border: Border.all(
+                    width: boxSize != null ? boxSize! / 15 : 1,
+                    color: value == name ? AppColor.primaryOne4B9EFF : disableColor ?? AppColor.inputBorderRegularE4E5E7,
+                  ),
+                  //color: circleClicked.value ? AppColor.secondaryFour877DF8 : AppColor.transparent,
+                ),
+                child: value == name
+                    ? Padding(
+                        padding: EdgeInsets.all(isIconRequired
+                            ? 0.0
+                            : boxSize != null
+                                ? boxSize! * 0.22
+                                : 4),
+                        child: Container(
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(isCheckbox ? 0 : 27),
+                            color: AppColor.primaryOne4B9EFF,
+                          ),
+                          child: isIconRequired
+                              ? Center(
+                                  child: Icon(
+                                    Icons.done,
+                                    size: boxSize != null ? boxSize! * 0.80 : 17,
+                                    color: AppColor.whiteFFFFFF,
+                                  ),
+                                )
+                              : const SizedBox.shrink(),
+                        ),
+                      )
+                    : Container(),
+              ),
+            ),
+          ),
+          AppGap.horizontal10,
+          titleChild?.call(value == name) ??
+              AppText.headline6(
+                title,
+                color: value == name ? AppColor.darkLight4D4D50 : disableColor ?? AppColor.inputBorderRegularE4E5E7,
+              )
+        ],
+      ),
+    );
+  }
+}