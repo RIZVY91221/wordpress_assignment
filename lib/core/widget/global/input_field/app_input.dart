RIZVY91221/simple_chat_app

ST-19: Create base input text field

lib/core/widget/global/input_field/app_input.dart
@@ -1,0 +1,195 @@
+import 'package:app_base_flutter/core/values/values.dart';
+import 'package:app_base_flutter/core/widget/global/input_field/wid_input_component.dart';
+import 'package:app_base_flutter/core/widget/global/input_field/wid_text_field.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+class BaseTextInput extends AppInputText {
+  const BaseTextInput({
+    Key? key,
+    FocusNode? focusNode,
+    double height = AppSize.s46,
+    int multiline = 1,
+    bool multilineAutoExpandable = false,
+    List<TextInputFormatter>? inputFormatters,
+    Widget? prefix,
+    Widget? preFIX,
+    double? preFIXWidth,
+    double? preFIXHeight,
+    double preFIXSideGap = AppSize.s18,
+    InputBorderStyle borderStyle = InputBorderStyle.bordered,
+    TextInputAction? textInputAction,
+    Widget? suffix,
+    Widget? sufFIX,
+    double? sufFIXWidth,
+    double? sufFIXHeight,
+    IconData? suffixIcon,
+    double? helperLeft,
+    String? labelText,
+    EdgeInsetsGeometry? labelPadding,
+    String? hintText,
+    String? errorText,
+    TextEditingController? controller,
+    bool obscureText = false,
+    bool autofocus = false,
+    bool error = false,
+    String? initialValue,
+    String? helperText,
+    String? counterText,
+    bool readOnly = false,
+    Color? fillColor,
+    TextAlign? textAlign,
+    TextInputType? keyboardType,
+    EdgeInsets? contentPadding,
+    bool enable = true,
+    String? Function(String?)? validator,
+    String? Function(String?)? onChanged,
+    String? Function()? onTap,
+    String? Function()? onEditingComplete,
+    String? Function(bool?)? onFocusChange,
+  }) : super(
+          key: key,
+          focusNode: focusNode,
+          height: height,
+          multiline: multiline,
+          multilineAutoExpandable: multilineAutoExpandable,
+          inputFormatters: inputFormatters,
+          prefix: prefix,
+          preFIX: preFIX,
+          preFIXWidth: preFIXWidth,
+          preFIXHeight: preFIXHeight,
+          preFIXSideGap: preFIXSideGap,
+          borderStyle: borderStyle,
+          textInputAction: textInputAction,
+          suffix: suffix,
+          sufFIX: sufFIX,
+          sufFIXWidth: sufFIXWidth,
+          sufFIXHeight: sufFIXHeight,
+          suffixIcon: suffixIcon,
+          helperLeft: helperLeft,
+          labelText: labelText,
+          labelPadding: labelPadding,
+          hintText: hintText,
+          errorText: errorText,
+          controller: controller,
+          obscureText: obscureText,
+          autofocus: autofocus,
+          error: error,
+          initialValue: initialValue,
+          helperText: helperText,
+          counterText: counterText,
+          readOnly: readOnly,
+          fillColor: fillColor,
+          textAlign: textAlign,
+          keyboardType: keyboardType,
+          contentPadding: contentPadding,
+          enable: enable,
+          validator: validator,
+          onChanged: onChanged,
+          onTap: onTap,
+          onEditingComplete: onEditingComplete,
+          onFocusChange: onFocusChange,
+        );
+}
+
+class BaseBorderlessInputText extends WidgetInputComponent {
+  const BaseBorderlessInputText({
+    Key? key,
+    String? value,
+    required String labelText,
+    String? hints,
+    String sideButtonText = 'Edit',
+    String sideButtonTextPressed = 'Save',
+    Widget? sideButton,
+    Color? sideButtonColor,
+    Widget? sideButtonPressed,
+    String? initialValue,
+    String? secondaryValue,
+    bool readOnly = true,
+    double dividerIndent = 0,
+    int? maxLength,
+    bool pageReadOnly = true,
+    EdgeInsetsGeometry padding = EdgeInsets.zero,
+    EdgeInsetsGeometry innerPadding = EdgeInsets.zero,
+    DataType dataType = DataType.text,
+    TextInputType? inputType,
+    Function()? onTap,
+    FocusNode? focusNode,
+    Function(String title, String value)? onEdit,
+    String? Function(bool?)? onFocusChange,
+  }) : super(
+          key: key,
+          labelText: labelText,
+          value: value,
+          hints: hints,
+          sideButtonText: sideButtonText,
+          sideButtonTextPressed: sideButtonTextPressed,
+          sideButton: sideButton,
+          sideButtonColor: sideButtonColor,
+          sideButtonPressed: sideButtonPressed,
+          initialValue: initialValue,
+          secondaryValue: secondaryValue,
+          readOnly: readOnly,
+          maxLength: maxLength,
+          dividerIndent: dividerIndent,
+          pageReadOnly: pageReadOnly,
+          padding: padding,
+          innerPadding: innerPadding,
+          dataType: dataType,
+          inputType: inputType,
+          onTap: onTap,
+          focusNode: focusNode,
+          onEdit: onEdit,
+        );
+}
+
+class BaseLongTextInput extends WidgetInputComponent {
+  const BaseLongTextInput({
+    Key? key,
+    String? value,
+    required String labelText,
+    String? hints,
+    String sideButtonText = 'Edit',
+    String sideButtonTextPressed = 'Save',
+    Widget? sideButton,
+    Color? sideButtonColor,
+    Widget? sideButtonPressed,
+    String? initialValue,
+    String? secondaryValue,
+    bool readOnly = true,
+    double dividerIndent = 0,
+    int maxLength = 300,
+    bool pageReadOnly = true,
+    EdgeInsetsGeometry padding = EdgeInsets.zero,
+    EdgeInsetsGeometry innerPadding = EdgeInsets.zero,
+    DataType dataType = DataType.text,
+    TextInputType? inputType,
+    Function()? onTap,
+    FocusNode? focusNode,
+    Function(String title, String value)? onEdit,
+    String? Function(bool?)? onFocusChange,
+  }) : super(
+          key: key,
+          labelText: labelText,
+          value: value,
+          hints: hints,
+          sideButtonText: sideButtonText,
+          sideButtonTextPressed: sideButtonTextPressed,
+          sideButton: sideButton,
+          sideButtonColor: sideButtonColor,
+          sideButtonPressed: sideButtonPressed,
+          initialValue: initialValue,
+          secondaryValue: secondaryValue,
+          readOnly: readOnly,
+          maxLength: maxLength,
+          dividerIndent: dividerIndent,
+          pageReadOnly: pageReadOnly,
+          padding: padding,
+          innerPadding: innerPadding,
+          dataType: dataType,
+          inputType: inputType,
+          onTap: onTap,
+          focusNode: focusNode,
+          onEdit: onEdit,
+        );
+}

lib/core/widget/global/text/wid_readmore_text.dart
@@ -1,0 +1,45 @@
+import 'package:app_base_flutter/core/theme/colors.dart';
+import 'package:flutter/material.dart';
+import 'package:readmore/readmore.dart';
+
+class WidgetReadMoreText extends StatelessWidget {
+  const WidgetReadMoreText(
+      {Key? key,
+      required this.text,
+      this.trimLines = 1,
+      this.preDataTextStyle,
+      this.textStyle,
+      this.moreStyle,
+      this.lessStyle,
+      this.clickableTextColor,
+      this.trimCollapsedText = 'More',
+      this.trimExpandedText = ' Less'})
+      : super(key: key);
+
+  final String text;
+  final int trimLines;
+  final TextStyle? preDataTextStyle;
+  final TextStyle? textStyle;
+  final TextStyle? moreStyle;
+  final TextStyle? lessStyle;
+  final Color? clickableTextColor;
+  final String trimCollapsedText;
+  final String trimExpandedText;
+
+  @override
+  Widget build(BuildContext context) {
+    return ReadMoreText(
+      text,
+      trimLines: trimLines,
+      preDataTextStyle: preDataTextStyle ?? Theme.of(context).textTheme.titleMedium,
+      style: textStyle ?? Theme.of(context).textTheme.titleMedium?.copyWith(color: AppColor.dark202125),
+      moreStyle: moreStyle ?? Theme.of(context).textTheme.titleMedium?.copyWith(color: Theme.of(context).primaryColor),
+      lessStyle: lessStyle ?? Theme.of(context).textTheme.titleMedium?.copyWith(color: Theme.of(context).primaryColor),
+      colorClickableText: clickableTextColor ?? Theme.of(context).primaryColor,
+      trimMode: TrimMode.Line,
+      trimCollapsedText: trimCollapsedText,
+      trimExpandedText: trimExpandedText,
+      textAlign: TextAlign.justify,
+    );
+  }
+}

RIZVY91221/wordpress_assignment

lib/core/widget/global/input_field/app_input.dart
@@ -1,0 +1,195 @@
+import 'package:bs_assignment/core/values/values.dart';
+import 'package:bs_assignment/core/widget/global/input_field/wid_input_component.dart';
+import 'package:bs_assignment/core/widget/global/input_field/wid_text_field.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+class BaseTextInput extends AppInputText {
+  const BaseTextInput({
+    Key? key,
+    FocusNode? focusNode,
+    double height = AppSize.s46,
+    int multiline = 1,
+    bool multilineAutoExpandable = false,
+    List<TextInputFormatter>? inputFormatters,
+    Widget? prefix,
+    Widget? preFIX,
+    double? preFIXWidth,
+    double? preFIXHeight,
+    double preFIXSideGap = AppSize.s18,
+    InputBorderStyle borderStyle = InputBorderStyle.bordered,
+    TextInputAction? textInputAction,
+    Widget? suffix,
+    Widget? sufFIX,
+    double? sufFIXWidth,
+    double? sufFIXHeight,
+    IconData? suffixIcon,
+    double? helperLeft,
+    String? labelText,
+    EdgeInsetsGeometry? labelPadding,
+    String? hintText,
+    String? errorText,
+    TextEditingController? controller,
+    bool obscureText = false,
+    bool autofocus = false,
+    bool error = false,
+    String? initialValue,
+    String? helperText,
+    String? counterText,
+    bool readOnly = false,
+    Color? fillColor,
+    TextAlign? textAlign,
+    TextInputType? keyboardType,
+    EdgeInsets? contentPadding,
+    bool enable = true,
+    String? Function(String?)? validator,
+    String? Function(String?)? onChanged,
+    String? Function()? onTap,
+    String? Function()? onEditingComplete,
+    String? Function(bool?)? onFocusChange,
+  }) : super(
+          key: key,
+          focusNode: focusNode,
+          height: height,
+          multiline: multiline,
+          multilineAutoExpandable: multilineAutoExpandable,
+          inputFormatters: inputFormatters,
+          prefix: prefix,
+          preFIX: preFIX,
+          preFIXWidth: preFIXWidth,
+          preFIXHeight: preFIXHeight,
+          preFIXSideGap: preFIXSideGap,
+          borderStyle: borderStyle,
+          textInputAction: textInputAction,
+          suffix: suffix,
+          sufFIX: sufFIX,
+          sufFIXWidth: sufFIXWidth,
+          sufFIXHeight: sufFIXHeight,
+          suffixIcon: suffixIcon,
+          helperLeft: helperLeft,
+          labelText: labelText,
+          labelPadding: labelPadding,
+          hintText: hintText,
+          errorText: errorText,
+          controller: controller,
+          obscureText: obscureText,
+          autofocus: autofocus,
+          error: error,
+          initialValue: initialValue,
+          helperText: helperText,
+          counterText: counterText,
+          readOnly: readOnly,
+          fillColor: fillColor,
+          textAlign: textAlign,
+          keyboardType: keyboardType,
+          contentPadding: contentPadding,
+          enable: enable,
+          validator: validator,
+          onChanged: onChanged,
+          onTap: onTap,
+          onEditingComplete: onEditingComplete,
+          onFocusChange: onFocusChange,
+        );
+}
+
+class BaseBorderlessInputText extends WidgetInputComponent {
+  const BaseBorderlessInputText({
+    Key? key,
+    String? value,
+    required String labelText,
+    String? hints,
+    String sideButtonText = 'Edit',
+    String sideButtonTextPressed = 'Save',
+    Widget? sideButton,
+    Color? sideButtonColor,
+    Widget? sideButtonPressed,
+    String? initialValue,
+    String? secondaryValue,
+    bool readOnly = true,
+    double dividerIndent = 0,
+    int? maxLength,
+    bool pageReadOnly = true,
+    EdgeInsetsGeometry padding = EdgeInsets.zero,
+    EdgeInsetsGeometry innerPadding = EdgeInsets.zero,
+    DataType dataType = DataType.text,
+    TextInputType? inputType,
+    Function()? onTap,
+    FocusNode? focusNode,
+    Function(String title, String value)? onEdit,
+    String? Function(bool?)? onFocusChange,
+  }) : super(
+          key: key,
+          labelText: labelText,
+          value: value,
+          hints: hints,
+          sideButtonText: sideButtonText,
+          sideButtonTextPressed: sideButtonTextPressed,
+          sideButton: sideButton,
+          sideButtonColor: sideButtonColor,
+          sideButtonPressed: sideButtonPressed,
+          initialValue: initialValue,
+          secondaryValue: secondaryValue,
+          readOnly: readOnly,
+          maxLength: maxLength,
+          dividerIndent: dividerIndent,
+          pageReadOnly: pageReadOnly,
+          padding: padding,
+          innerPadding: innerPadding,
+          dataType: dataType,
+          inputType: inputType,
+          onTap: onTap,
+          focusNode: focusNode,
+          onEdit: onEdit,
+        );
+}
+
+class BaseLongTextInput extends WidgetInputComponent {
+  const BaseLongTextInput({
+    Key? key,
+    String? value,
+    required String labelText,
+    String? hints,
+    String sideButtonText = 'Edit',
+    String sideButtonTextPressed = 'Save',
+    Widget? sideButton,
+    Color? sideButtonColor,
+    Widget? sideButtonPressed,
+    String? initialValue,
+    String? secondaryValue,
+    bool readOnly = true,
+    double dividerIndent = 0,
+    int maxLength = 300,
+    bool pageReadOnly = true,
+    EdgeInsetsGeometry padding = EdgeInsets.zero,
+    EdgeInsetsGeometry innerPadding = EdgeInsets.zero,
+    DataType dataType = DataType.text,
+    TextInputType? inputType,
+    Function()? onTap,
+    FocusNode? focusNode,
+    Function(String title, String value)? onEdit,
+    String? Function(bool?)? onFocusChange,
+  }) : super(
+          key: key,
+          labelText: labelText,
+          value: value,
+          hints: hints,
+          sideButtonText: sideButtonText,
+          sideButtonTextPressed: sideButtonTextPressed,
+          sideButton: sideButton,
+          sideButtonColor: sideButtonColor,
+          sideButtonPressed: sideButtonPressed,
+          initialValue: initialValue,
+          secondaryValue: secondaryValue,
+          readOnly: readOnly,
+          maxLength: maxLength,
+          dividerIndent: dividerIndent,
+          pageReadOnly: pageReadOnly,
+          padding: padding,
+          innerPadding: innerPadding,
+          dataType: dataType,
+          inputType: inputType,
+          onTap: onTap,
+          focusNode: focusNode,
+          onEdit: onEdit,
+        );
+}

lib/core/widget/global/text/wid_readmore_text.dart
@@ -1,0 +1,45 @@
+import 'package:bs_assignment/core/theme/colors.dart';
+import 'package:flutter/material.dart';
+import 'package:readmore/readmore.dart';
+
+class WidgetReadMoreText extends StatelessWidget {
+  const WidgetReadMoreText(
+      {Key? key,
+      required this.text,
+      this.trimLines = 1,
+      this.preDataTextStyle,
+      this.textStyle,
+      this.moreStyle,
+      this.lessStyle,
+      this.clickableTextColor,
+      this.trimCollapsedText = 'More',
+      this.trimExpandedText = ' Less'})
+      : super(key: key);
+
+  final String text;
+  final int trimLines;
+  final TextStyle? preDataTextStyle;
+  final TextStyle? textStyle;
+  final TextStyle? moreStyle;
+  final TextStyle? lessStyle;
+  final Color? clickableTextColor;
+  final String trimCollapsedText;
+  final String trimExpandedText;
+
+  @override
+  Widget build(BuildContext context) {
+    return ReadMoreText(
+      text,
+      trimLines: trimLines,
+      preDataTextStyle: preDataTextStyle ?? Theme.of(context).textTheme.titleMedium,
+      style: textStyle ?? Theme.of(context).textTheme.titleMedium?.copyWith(color: AppColor.dark202125),
+      moreStyle: moreStyle ?? Theme.of(context).textTheme.titleMedium?.copyWith(color: Theme.of(context).primaryColor),
+      lessStyle: lessStyle ?? Theme.of(context).textTheme.titleMedium?.copyWith(color: Theme.of(context).primaryColor),
+      colorClickableText: clickableTextColor ?? Theme.of(context).primaryColor,
+      trimMode: TrimMode.Line,
+      trimCollapsedText: trimCollapsedText,
+      trimExpandedText: trimExpandedText,
+      textAlign: TextAlign.justify,
+    );
+  }
+}