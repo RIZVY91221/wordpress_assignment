RIZVY91221/simple_chat_app

ST-19: Create datetime input field

lib/core/widget/global/dailog/wid_date_time_dialogue.dart
@@ -1,0 +1,24 @@
+import 'package:app_base_flutter/core/widget/global/dailog/wid_dialog.dart';
+import 'package:app_base_flutter/core/widget/global/date_time/wid_date_time.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+Future<void> datePickerDialogue({Function(DateTime?)? onChange, DateTime? initialDate, bool? isBefore}) async {
+  await appDialogue(
+    height: 405,
+    padding: const EdgeInsets.only(top: 5),
+    child: AppDateTime(
+      initialDate: initialDate,
+      onChange: (value) {
+        onChange?.call(value);
+        Get.back();
+      },
+      onPressToday: (value) {
+        onChange?.call(value);
+        Get.back();
+      },
+      isBefore: isBefore,
+    ),
+    barrierDismissible: true,
+  );
+}

lib/core/widget/global/dailog/wid_dialog.dart
@@ -1,0 +1,51 @@
+import 'dart:ui';
+
+import 'package:app_base_flutter/core/theme/colors.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+Future appDialogue({
+  double height = 285,
+  double borderRadius = 24,
+  double elevation = 10,
+  Widget? child,
+  EdgeInsets? insetPadding,
+  EdgeInsets? padding,
+  Color? backgroundColor,
+  Color? shadowColor,
+  bool barrierDismissible = false,
+  bool backgroundBlur = false,
+}) {
+  return showCupertinoDialog(
+    context: Get.context!,
+    barrierDismissible: barrierDismissible,
+    barrierLabel: MaterialLocalizations.of(Get.context!).modalBarrierDismissLabel,
+    builder: (BuildContext context) {
+      return Dialog(
+        insetPadding: insetPadding ?? const EdgeInsets.symmetric(horizontal: 10),
+        shadowColor: shadowColor ?? Colors.white,
+        surfaceTintColor: backgroundColor,
+        backgroundColor: backgroundColor ?? AppColor.whiteFFFFFF,
+        elevation: elevation,
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(borderRadius)),
+        child: BackdropFilter(
+          filter: ImageFilter.blur(
+            sigmaX: backgroundBlur ? 15 : 0,
+            sigmaY: backgroundBlur ? 15 : 0,
+          ),
+          child: ClipRRect(
+            borderRadius: BorderRadius.circular(borderRadius),
+            child: Container(
+              color: backgroundColor ?? Colors.white,
+              height: height,
+              width: double.infinity,
+              padding: padding ?? const EdgeInsets.fromLTRB(24.0, 24.0, 24.0, 36.0),
+              child: child,
+            ),
+          ),
+        ),
+      );
+    },
+  );
+}

lib/core/widget/global/date_time/wid_date_time.dart
@@ -1,0 +1,72 @@
+import 'package:app_base_flutter/core/theme/text.dart';
+import 'package:app_base_flutter/core/widget/global/button/wid_button_static.dart';
+import 'package:app_base_flutter/core/widget/global/divider/wid_divider.dart';
+import 'package:calendar_date_picker2/calendar_date_picker2.dart';
+import 'package:flutter/material.dart';
+
+class AppDateTime extends StatefulWidget {
+  const AppDateTime({Key? key, required this.onChange, this.onPressToday, this.initialDate, this.isBefore}) : super(key: key);
+
+  final Function(DateTime?) onChange;
+  final Function(DateTime?)? onPressToday;
+  final DateTime? initialDate;
+  final bool? isBefore;
+
+  @override
+  State<AppDateTime> createState() => _AppDateTimeState();
+}
+
+class _AppDateTimeState extends State<AppDateTime> {
+  DateTime? dateTime;
+  DateTime currentDate = DateTime.now();
+
+  bool _isDateSelectable(DateTime day) {
+    if (widget.isBefore != null) {
+      if (widget.isBefore == true) {
+        return day.isBefore(widget.initialDate ?? currentDate);
+      } else {
+        return day.isAfter(widget.initialDate ?? currentDate);
+      }
+    } else {
+      return true;
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        CalendarDatePicker2(
+          config: CalendarDatePicker2Config(
+            calendarType: CalendarDatePicker2Type.single,
+            selectableDayPredicate: _isDateSelectable,
+            centerAlignModePicker: true,
+            selectedDayHighlightColor: Theme.of(context).primaryColor,
+            dayBorderRadius: BorderRadius.circular(0),
+            // currentDate: currentDate,
+          ),
+          value: [widget.initialDate],
+          onValueChanged: (value) {
+            currentDate = value[0] ?? DateTime.now();
+            widget.onChange.call(currentDate);
+          },
+        ),
+        const AppDivider(),
+        AppStaticButton(
+          height: 50,
+          child: AppText.link('Today'),
+          onPressed: () {
+            setState(() => currentDate = DateTime.now());
+            widget.onPressToday?.call(currentDate);
+          },
+        ),
+      ],
+    );
+  }
+
+  @override
+  void initState() {
+    if (widget.initialDate != null) currentDate = widget.initialDate!;
+    super.initState();
+  }
+}

lib/core/widget/global/input_field/wid_input_component.dart
@@ -5,17 +5,14 @@ import 'package:app_base_flutter/core/uttils/extensions.dart';
 import 'package:app_base_flutter/core/uttils/icons.dart';
 import 'package:app_base_flutter/core/uttils/toasts.dart';
 import 'package:app_base_flutter/core/values/values.dart';
+import 'package:app_base_flutter/core/widget/global/dailog/wid_date_time_dialogue.dart';
 import 'package:app_base_flutter/core/widget/global/divider/wid_divider.dart';
 import 'package:app_base_flutter/core/widget/global/text/wid_readmore_text.dart';
 import 'package:app_base_flutter/generated/assets.dart';
 import 'package:expandable/expandable.dart';
 import 'package:flutter/material.dart';
 
-enum DataType {
-  text,
-  tap,
-  dropDown,
-}
+enum DataType { text, tap, dropDown, date }
 
 /// DropDown Small Model
 class DropdownModel {
@@ -45,6 +42,8 @@ class WidgetInputComponent extends StatefulWidget {
     this.value,
     this.initialValue,
     this.secondaryValue,
+    this.dateTimeFormat = 'dd MMM yy, hh:mm',
+    this.disableDate,
     this.readOnly = true,
     this.dividerIndent = 0,
     this.maxLength,
@@ -71,6 +70,8 @@ class WidgetInputComponent extends StatefulWidget {
   final Widget? sideButtonPressed;
   final String? initialValue;
   final String? secondaryValue;
+  final String dateTimeFormat;
+  final bool? disableDate;
   final bool readOnly;
   final double dividerIndent;
   final bool pageReadOnly;
@@ -138,6 +139,9 @@ class _WidgetInputComponentState extends State<WidgetInputComponent> {
                             if (widget.dataType == DataType.dropDown) {
                               controllerExpandable.expanded = dropdownExpansion = !dropdownExpansion;
                             }
+                          }
+                          if (widget.dataType == DataType.date) {
+                            dateTimeDialog();
                           }
                         },
                   child: Row(
@@ -317,6 +321,9 @@ class _WidgetInputComponentState extends State<WidgetInputComponent> {
         if (widget.dataType == DataType.dropDown) {
           controllerExpandable.expanded = dropdownExpansion = !dropdownExpansion;
         }
+        if (widget.dataType == DataType.date) {
+          dateTimeDialog();
+        }
       },
       child: enable ? widgetOnSave() : buttonWidget(widgetOnEdit()),
     );
@@ -340,6 +347,8 @@ class _WidgetInputComponentState extends State<WidgetInputComponent> {
         return widget.sideButton ?? _editSaveText(widget.sideButtonText);
       case DataType.dropDown:
         return widget.sideButton ?? _editSaveText(widget.sideButtonText);
+      case DataType.date:
+        return widget.sideButton ?? _editSaveText(widget.sideButtonText);
       case DataType.tap:
         return widget.sideButton ?? appSVG(Assets.svgLeftChevron);
     }
@@ -352,9 +361,36 @@ class _WidgetInputComponentState extends State<WidgetInputComponent> {
         return widget.sideButtonPressed ?? const SizedBox.shrink(); //_editSaveText(widget.sideButtonTextPressed);
       case DataType.dropDown:
         return widget.sideButtonPressed ?? Icon(Icons.keyboard_arrow_down_rounded, size: 24, color: AppColor.inputBorderRegularE4E5E7);
+      case DataType.date:
+        return widget.sideButtonPressed ?? Icon(Icons.calendar_month, size: 18, color: widget.sideButtonColor ?? Colors.black);
       case DataType.tap:
         return widget.sideButtonPressed ?? appSVG(Assets.svgLeftChevron, color: widget.sideButtonColor);
     }
+  }
+
+  void dateTimeDialog() async {
+    DateTime initialDate = DateTime.now();
+
+    if (widget.secondaryValue != null) {
+      if (widget.secondaryValue != 'null') initialDate = DateTime.parse(widget.secondaryValue!);
+    }
+
+    await datePickerDialogue(
+        initialDate: initialDate,
+        onChange: (value) {
+          if (value != null) {
+            bool isEqual = (textTitle == '$value'.dateTimeFormat(format: widget.dateTimeFormat));
+            if (isEqual) {
+              AppToasts.error(msg: 'Select different date');
+            } else {
+              setState(() => textTitle = '$value'.dateTimeFormat(format: widget.dateTimeFormat));
+              textValue = '$value';
+              widget.onEdit?.call(textTitle, textValue);
+            }
+          }
+        },
+        isBefore: widget.disableDate);
+    setState(() => enable = !enable);
   }
 
   /// Divider

RIZVY91221/wordpress_assignment

lib/core/widget/global/dailog/wid_date_time_dialogue.dart
@@ -1,0 +1,24 @@
+import 'package:bs_assignment/core/widget/global/dailog/wid_dialog.dart';
+import 'package:bs_assignment/core/widget/global/date_time/wid_date_time.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+Future<void> datePickerDialogue({Function(DateTime?)? onChange, DateTime? initialDate, bool? isBefore}) async {
+  await appDialogue(
+    height: 405,
+    padding: const EdgeInsets.only(top: 5),
+    child: AppDateTime(
+      initialDate: initialDate,
+      onChange: (value) {
+        onChange?.call(value);
+        Get.back();
+      },
+      onPressToday: (value) {
+        onChange?.call(value);
+        Get.back();
+      },
+      isBefore: isBefore,
+    ),
+    barrierDismissible: true,
+  );
+}

lib/core/widget/global/dailog/wid_dialog.dart
@@ -1,0 +1,51 @@
+import 'dart:ui';
+
+import 'package:bs_assignment/core/theme/colors.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+Future appDialogue({
+  double height = 285,
+  double borderRadius = 24,
+  double elevation = 10,
+  Widget? child,
+  EdgeInsets? insetPadding,
+  EdgeInsets? padding,
+  Color? backgroundColor,
+  Color? shadowColor,
+  bool barrierDismissible = false,
+  bool backgroundBlur = false,
+}) {
+  return showCupertinoDialog(
+    context: Get.context!,
+    barrierDismissible: barrierDismissible,
+    barrierLabel: MaterialLocalizations.of(Get.context!).modalBarrierDismissLabel,
+    builder: (BuildContext context) {
+      return Dialog(
+        insetPadding: insetPadding ?? const EdgeInsets.symmetric(horizontal: 10),
+        shadowColor: shadowColor ?? Colors.white,
+        surfaceTintColor: backgroundColor,
+        backgroundColor: backgroundColor ?? AppColor.whiteFFFFFF,
+        elevation: elevation,
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(borderRadius)),
+        child: BackdropFilter(
+          filter: ImageFilter.blur(
+            sigmaX: backgroundBlur ? 15 : 0,
+            sigmaY: backgroundBlur ? 15 : 0,
+          ),
+          child: ClipRRect(
+            borderRadius: BorderRadius.circular(borderRadius),
+            child: Container(
+              color: backgroundColor ?? Colors.white,
+              height: height,
+              width: double.infinity,
+              padding: padding ?? const EdgeInsets.fromLTRB(24.0, 24.0, 24.0, 36.0),
+              child: child,
+            ),
+          ),
+        ),
+      );
+    },
+  );
+}

lib/core/widget/global/date_time/wid_date_time.dart
@@ -1,0 +1,72 @@
+import 'package:bs_assignment/core/theme/text.dart';
+import 'package:bs_assignment/core/widget/global/button/wid_button_static.dart';
+import 'package:bs_assignment/core/widget/global/divider/wid_divider.dart';
+import 'package:calendar_date_picker2/calendar_date_picker2.dart';
+import 'package:flutter/material.dart';
+
+class AppDateTime extends StatefulWidget {
+  const AppDateTime({Key? key, required this.onChange, this.onPressToday, this.initialDate, this.isBefore}) : super(key: key);
+
+  final Function(DateTime?) onChange;
+  final Function(DateTime?)? onPressToday;
+  final DateTime? initialDate;
+  final bool? isBefore;
+
+  @override
+  State<AppDateTime> createState() => _AppDateTimeState();
+}
+
+class _AppDateTimeState extends State<AppDateTime> {
+  DateTime? dateTime;
+  DateTime currentDate = DateTime.now();
+
+  bool _isDateSelectable(DateTime day) {
+    if (widget.isBefore != null) {
+      if (widget.isBefore == true) {
+        return day.isBefore(widget.initialDate ?? currentDate);
+      } else {
+        return day.isAfter(widget.initialDate ?? currentDate);
+      }
+    } else {
+      return true;
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        CalendarDatePicker2(
+          config: CalendarDatePicker2Config(
+            calendarType: CalendarDatePicker2Type.single,
+            selectableDayPredicate: _isDateSelectable,
+            centerAlignModePicker: true,
+            selectedDayHighlightColor: Theme.of(context).primaryColor,
+            dayBorderRadius: BorderRadius.circular(0),
+            // currentDate: currentDate,
+          ),
+          value: [widget.initialDate],
+          onValueChanged: (value) {
+            currentDate = value[0] ?? DateTime.now();
+            widget.onChange.call(currentDate);
+          },
+        ),
+        const AppDivider(),
+        AppStaticButton(
+          height: 50,
+          child: AppText.link('Today'),
+          onPressed: () {
+            setState(() => currentDate = DateTime.now());
+            widget.onPressToday?.call(currentDate);
+          },
+        ),
+      ],
+    );
+  }
+
+  @override
+  void initState() {
+    if (widget.initialDate != null) currentDate = widget.initialDate!;
+    super.initState();
+  }
+}

lib/core/widget/global/input_field/wid_input_component.dart
@@ -4,6 +4,7 @@ import 'package:bs_assignment/core/theme/text.dart';
 import 'package:bs_assignment/core/uttils/icons.dart';
 import 'package:bs_assignment/core/uttils/toasts.dart';
 import 'package:bs_assignment/core/values/values.dart';
+import 'package:bs_assignment/core/widget/global/dailog/wid_date_time_dialogue.dart';
 import 'package:bs_assignment/core/widget/global/divider/wid_divider.dart';
 import 'package:bs_assignment/core/widget/global/text/wid_readmore_text.dart';
 import 'package:bs_assignment/core/utils/extensions.dart';
@@ -11,11 +12,7 @@ import 'package:bs_assignment/generated/assets.dart';
 import 'package:expandable/expandable.dart';
 import 'package:flutter/material.dart';
 
-enum DataType {
-  text,
-  tap,
-  dropDown,
-}
+enum DataType { text, tap, dropDown, date }
 
 /// DropDown Small Model
 class DropdownModel {
@@ -45,6 +42,8 @@ class WidgetInputComponent extends StatefulWidget {
     this.value,
     this.initialValue,
     this.secondaryValue,
+    this.dateTimeFormat = 'dd MMM yy, hh:mm',
+    this.disableDate,
     this.readOnly = true,
     this.dividerIndent = 0,
     this.maxLength,
@@ -71,6 +70,8 @@ class WidgetInputComponent extends StatefulWidget {
   final Widget? sideButtonPressed;
   final String? initialValue;
   final String? secondaryValue;
+  final String dateTimeFormat;
+  final bool? disableDate;
   final bool readOnly;
   final double dividerIndent;
   final bool pageReadOnly;
@@ -138,6 +139,9 @@ class _WidgetInputComponentState extends State<WidgetInputComponent> {
                             if (widget.dataType == DataType.dropDown) {
                               controllerExpandable.expanded = dropdownExpansion = !dropdownExpansion;
                             }
+                          }
+                          if (widget.dataType == DataType.date) {
+                            dateTimeDialog();
                           }
                         },
                   child: Row(
@@ -317,6 +321,9 @@ class _WidgetInputComponentState extends State<WidgetInputComponent> {
         if (widget.dataType == DataType.dropDown) {
           controllerExpandable.expanded = dropdownExpansion = !dropdownExpansion;
         }
+        if (widget.dataType == DataType.date) {
+          dateTimeDialog();
+        }
       },
       child: enable ? widgetOnSave() : buttonWidget(widgetOnEdit()),
     );
@@ -340,6 +347,8 @@ class _WidgetInputComponentState extends State<WidgetInputComponent> {
         return widget.sideButton ?? _editSaveText(widget.sideButtonText);
       case DataType.dropDown:
         return widget.sideButton ?? _editSaveText(widget.sideButtonText);
+      case DataType.date:
+        return widget.sideButton ?? _editSaveText(widget.sideButtonText);
       case DataType.tap:
         return widget.sideButton ?? appSVG(Assets.svgLeftChevron);
     }
@@ -352,9 +361,36 @@ class _WidgetInputComponentState extends State<WidgetInputComponent> {
         return widget.sideButtonPressed ?? const SizedBox.shrink(); //_editSaveText(widget.sideButtonTextPressed);
       case DataType.dropDown:
         return widget.sideButtonPressed ?? Icon(Icons.keyboard_arrow_down_rounded, size: 24, color: AppColor.inputBorderRegularE4E5E7);
+      case DataType.date:
+        return widget.sideButtonPressed ?? Icon(Icons.calendar_month, size: 18, color: widget.sideButtonColor ?? Colors.black);
       case DataType.tap:
         return widget.sideButtonPressed ?? appSVG(Assets.svgLeftChevron, color: widget.sideButtonColor);
     }
+  }
+
+  void dateTimeDialog() async {
+    DateTime initialDate = DateTime.now();
+
+    if (widget.secondaryValue != null) {
+      if (widget.secondaryValue != 'null') initialDate = DateTime.parse(widget.secondaryValue!);
+    }
+
+    await datePickerDialogue(
+        initialDate: initialDate,
+        onChange: (value) {
+          if (value != null) {
+            bool isEqual = (textTitle == '$value'.dateTimeFormat(format: widget.dateTimeFormat));
+            if (isEqual) {
+              AppToasts.error(msg: 'Select different date');
+            } else {
+              setState(() => textTitle = '$value'.dateTimeFormat(format: widget.dateTimeFormat));
+              textValue = '$value';
+              widget.onEdit?.call(textTitle, textValue);
+            }
+          }
+        },
+        isBefore: widget.disableDate);
+    setState(() => enable = !enable);
   }
 
   /// Divider