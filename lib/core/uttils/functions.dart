RIZVY91221/simple_chat_app

ST-14: Card helper widget

lib/core/uttils/functions.dart
@@ -1,0 +1,230 @@
+import 'dart:convert';
+import 'dart:io';
+
+import 'package:app_base_flutter/core/theme/colors.dart';
+import 'package:app_base_flutter/core/theme/text.dart';
+import 'package:app_base_flutter/core/uttils/extensions.dart';
+import 'package:app_base_flutter/core/uttils/icons.dart';
+import 'package:app_base_flutter/core/widget/global/text/wid_icon_text.dart';
+import 'package:app_base_flutter/environment/build_config.dart';
+import 'package:app_base_flutter/generated/assets.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:get/get.dart';
+
+/* Debug Print */
+appPrint(dynamic data) => BuildConfig.instance.envVariables.shouldCollectCrashLog ? debugPrint('$data') : '';
+
+class FN extends Deprecated {
+  FN(super.message);
+
+  /* Debug Print */
+  @Deprecated('Use "appPrint()" instead')
+  static print(dynamic data) => appPrint(data);
+
+  /* Combine Error Messages */
+  static String getErrorMessageCombined(Map<String, dynamic> errors) {
+    int index = 0;
+    String message = '';
+
+    errors.forEach((key, value) {
+      index++;
+      message += value[0];
+      if (errors.length != index) {
+        message += '\n';
+      }
+    });
+    return message;
+  }
+
+  /* ***
+   * Convert Base 64 Images into Viewable Widget
+   * **/
+
+  static Widget imageWidgetFromBase64(
+    String stringBase64, {
+    String filterOutBefore = '',
+    String filterOutAfter = '',
+    double? height,
+    double? width,
+    BoxFit? fit,
+    Color? color,
+    Color? textColor,
+    Widget? onErrorWidget,
+    Widget? onErrorPlaceholderWidget,
+    AlignmentGeometry? alignment,
+    BlendMode? colorBlendMode,
+    bool onErrorDefaultPlaceholder = false,
+    bool isText = false,
+    double borderRadius = 3,
+    Function(Object, StackTrace?)? onError,
+  }) {
+    Container box({Widget? child}) => Container(
+          height: height,
+          width: width,
+          color: color,
+          decoration: BoxDecoration(
+            color: color,
+            borderRadius: BorderRadius.circular(borderRadius),
+          ),
+          child: child,
+        );
+
+    Image noImage = Image.asset(Assets.imagesNoImage);
+
+    /// Check Blank
+    if (stringBase64.isEmpty || stringBase64 == 'null') return box(child: onErrorWidget ?? noImage);
+
+    /// When Widget is Text
+    if (isText) {
+      appPrint('⇢ ⇢ ⇢ Text:  "String"');
+      return WidgetIconWithLetter(
+        text: stringBase64,
+        height: height,
+        width: width,
+        bgColor: color,
+        textColor: textColor,
+        borderRadius: borderRadius,
+      );
+    }
+
+    /// When None of Above
+    Uint8List? bytes;
+
+    try {
+      bytes = getFromBase64(
+        stringBase64,
+        filterOutBefore: filterOutBefore,
+        filterOutAfter: filterOutAfter,
+      );
+    } catch (_) {}
+
+    Image? image;
+
+    Widget? svgImage;
+
+    String initializer({int char = 4}) => stringBase64.substring(0, char);
+
+    String extension() {
+      String lastTen = stringBase64.substring(stringBase64.length - 10, stringBase64.length);
+      String ext = 'base64';
+      try {
+        ext = lastTen.split('.')[1];
+      } catch (_) {}
+      return ext;
+    }
+
+    Widget errorWidget() => (onErrorDefaultPlaceholder ? noImage : (onErrorPlaceholderWidget ?? const SizedBox.shrink()));
+
+    appPrint('⇢ ⇢ ⇢ Image: "${initializer(char: 6)}◦◦◦.${extension()}"');
+
+    if (extension() == 'svg') {
+      if (initializer() == 'http') {
+        svgImage = appSVGNetworkComplex(
+          stringBase64,
+          color: color,
+          width: width,
+          height: height,
+        );
+      } else {
+        svgImage = appSVG(
+          stringBase64,
+          color: color,
+          width: width,
+          height: height,
+        );
+      }
+    } else {
+      if (initializer() == 'http') {
+        image = Image.network(
+          stringBase64,
+          fit: fit,
+          errorBuilder: (_, obj, trace) {
+            onError?.call(obj, trace);
+            return errorWidget();
+          },
+        );
+      } else if (initializer(char: 6) == 'assets') {
+        image = Image.asset(
+          stringBase64,
+          fit: fit,
+          errorBuilder: (_, obj, trace) {
+            onError?.call(obj, trace);
+            return errorWidget();
+          },
+        );
+      } else {
+        image = Image.file(
+          File(stringBase64),
+          fit: fit,
+          errorBuilder: (_, obj, trace) {
+            onError?.call(obj, trace);
+            return errorWidget();
+          },
+        );
+      }
+    }
+
+    return bytes != null
+        ? Image.memory(
+            bytes,
+            height: height,
+            width: width,
+            fit: fit,
+            color: color,
+            alignment: alignment ?? Alignment.center,
+            colorBlendMode: colorBlendMode,
+          )
+        : box(child: onErrorWidget ?? image ?? svgImage);
+  }
+
+  /* ***
+   * Decode Base 64 Images as "Uint8List"
+   * Have to view in "Image.memory()" Widget
+   * **/
+
+  static Uint8List getFromBase64(
+    String stringBase64, {
+    String filterOutBefore = '',
+    String filterOutAfter = '',
+  }) {
+    late Uint8List bytes;
+    if (filterOutBefore.isNotEmpty || filterOutAfter.isNotEmpty) {
+      bytes = base64.decode(stringBase64.toSubstring(fromStart: filterOutBefore.length, fromEnd: filterOutAfter.length));
+    } else {
+      bytes = base64.decode(stringBase64.split(';base64,').last);
+    }
+    return bytes;
+  }
+
+  static String getStatus(dynamic item) {
+    if (item != null) {
+      return item.map((e) => e is String ? e : e["name"]).toList().join(", ").toString();
+    } else {
+      return '-';
+    }
+  }
+
+  static copyText(String text) async {
+    ClipboardData? data = await Clipboard.getData('text/plain');
+    var copiedText = data?.text;
+    if (copiedText?.trim() == text.trim()) {
+      return;
+    } else {
+      Clipboard.setData(ClipboardData(text: text)).then((_) {
+        Get.rawSnackbar(
+            messageText: AppText.bodyExtraSmall("Copied", textAlign: TextAlign.center, color: AppColor.dark202125),
+            padding: const EdgeInsets.all(5),
+            backgroundColor: Colors.grey.withOpacity(0.2),
+            animationDuration: const Duration(milliseconds: 500),
+            duration: const Duration(milliseconds: 1000));
+      });
+    }
+  }
+
+  static dynamic getCopyText() async {
+    ClipboardData? data = await Clipboard.getData('text/plain');
+    var text = data?.text;
+    return text;
+  }
+}

lib/core/widget/global/card/card_item.dart
@@ -1,0 +1,84 @@
+import 'package:app_base_flutter/core/theme/colors.dart';
+import 'package:app_base_flutter/core/theme/text.dart';
+import 'package:flutter/material.dart';
+
+class ItemCard extends StatefulWidget {
+  const ItemCard(
+      {super.key,
+      required this.detailsKey,
+      this.detailsValue = '',
+      this.titleColor,
+      this.isExpand = true,
+      this.valueColor,
+      this.valueChild});
+
+  final String detailsKey;
+  final String detailsValue;
+  final Color? titleColor;
+  final Color? valueColor;
+  final bool? isExpand;
+  final Widget? valueChild;
+
+  @override
+  State<ItemCard> createState() => _ItemCardState();
+}
+
+class _ItemCardState extends State<ItemCard> {
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisSize: MainAxisSize.max,
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        if (widget.detailsKey != '') AppText.bodyVerySmall(widget.detailsKey, color: widget.titleColor ?? AppColor.darkLight4D4D50),
+        if (widget.detailsValue != '')
+          !widget.isExpand!
+              ? widget.detailsValue.contains("Canceled")
+                  ? CanceledValueChecker(
+                      value: widget.detailsValue,
+                    )
+                  : AppText.bodyVerySmall(widget.detailsValue, color: widget.valueColor ?? AppColor.dark202125)
+              : Expanded(
+                  child: widget.detailsValue.contains("Canceled")
+                      ? CanceledValueChecker(
+                          value: widget.detailsValue,
+                        )
+                      : AppText.bodyVerySmall(widget.detailsValue, color: widget.valueColor ?? AppColor.dark202125),
+                ),
+        if (widget.valueChild != null)
+          Padding(
+            padding: const EdgeInsets.only(left: 3.0),
+            child: widget.valueChild!,
+          )
+      ],
+    );
+  }
+}
+
+class CanceledValueChecker extends StatelessWidget {
+  const CanceledValueChecker({Key? key, required this.value}) : super(key: key);
+
+  final String value;
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+        children: value
+            .split(',')
+            .toList()
+            .asMap()
+            .map((index, e) => MapEntry(
+                index,
+                Row(
+                  children: [
+                    AppText.bodyVerySmall(e.contains("Canceled") ? "Cancelled" : e,
+                        color: e.contains("Canceled") ? AppColor.errorFE6C44 : AppColor.dark202125),
+                    index == value.split(',').toList().length - 1
+                        ? const SizedBox.shrink()
+                        : AppText.bodyVerySmall(',', color: AppColor.dark202125)
+                  ],
+                )))
+            .values
+            .toList());
+  }
+}

lib/core/widget/global/card/card_title.dart
@@ -1,0 +1,106 @@
+import 'package:app_base_flutter/core/theme/colors.dart';
+import 'package:app_base_flutter/core/theme/text.dart';
+import 'package:app_base_flutter/core/uttils/functions.dart';
+import 'package:app_base_flutter/core/uttils/icons.dart';
+import 'package:app_base_flutter/generated/assets.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+enum IconDirection { left, right }
+
+// ignore: must_be_immutable
+class CardTitleWithIcon extends StatelessWidget {
+  CardTitleWithIcon({
+    Key? key,
+    this.storeIcon,
+    required this.title,
+    this.textAsTitle = true,
+    this.showCopyButton = true,
+    this.iconDirection = IconDirection.left,
+    this.mainAxisSize = MainAxisSize.max,
+  }) : super(key: key);
+
+  final String? storeIcon;
+  final String title;
+  final bool textAsTitle;
+  final bool showCopyButton;
+  final IconDirection iconDirection;
+  final MainAxisSize mainAxisSize;
+
+  var copiedDone = false.obs;
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisSize: mainAxisSize,
+      children: [
+        iconWidget(iconDirection == IconDirection.left),
+        orderIdText(),
+        iconWidget(iconDirection == IconDirection.right),
+        if (showCopyButton) copyButtonWidget(),
+      ],
+    );
+  }
+
+  /// Icon Widget
+  Widget iconWidget(bool show) => (show && storeIcon != null)
+      ? Padding(
+          padding: const EdgeInsets.only(right: 5),
+          child: ClipRRect(
+            borderRadius: const BorderRadius.all(Radius.circular(4)),
+            child: FN.imageWidgetFromBase64(
+              storeIcon!.isNotEmpty ? storeIcon! : title.characters.elementAt(1),
+              fit: BoxFit.cover,
+              isText: storeIcon!.isEmpty ? true : false,
+              width: storeIcon!.isNotEmpty ? 18 : 22,
+              height: storeIcon!.isNotEmpty ? 18 : 22,
+            ),
+          ))
+      : const SizedBox.shrink();
+
+  /// Order IdD Text Widget
+  Padding orderIdText() {
+    return Padding(
+      padding: const EdgeInsets.only(right: 5),
+      child: textAsTitle ? AppText.bodySmall(title, color: AppColor.dark202125) : AppText.bodyVerySmall(title, color: AppColor.dark202125),
+    );
+  }
+
+  /// Order No Widget
+  Obx copyButtonWidget() {
+    return Obx(
+      () => InkWell(
+        splashColor: Colors.transparent,
+        highlightColor: Colors.transparent,
+        onTap: () async {
+          await FN.copyText(title);
+          copiedDone.value = true;
+          Future.delayed(const Duration(seconds: 2), () {
+            copiedDone.value = false;
+          });
+        },
+        child: SizedBox(
+          height: 15,
+          width: 40,
+          child: copiedDone.value
+              ? Align(
+                  alignment: Alignment.centerLeft,
+                  child: Icon(
+                    Icons.check,
+                    color: AppColor.darkLightest6C7576,
+                    size: 14,
+                  ),
+                )
+              : Align(
+                  alignment: Alignment.centerLeft,
+                  child: appSVG(
+                    Assets.svgCopyIcon,
+                    color: AppColor.darkLightest6C7576,
+                    width: 14,
+                    height: 18,
+                  ),
+                ),
+        ),
+      ),
+    );
+  }
+}

lib/core/widget/global/text/wid_icon_text.dart
@@ -1,0 +1,47 @@
+import 'package:app_base_flutter/core/theme/colors.dart';
+import 'package:app_base_flutter/core/theme/text.dart';
+import 'package:flutter/material.dart';
+
+class WidgetIconWithLetter extends StatelessWidget {
+  const WidgetIconWithLetter({
+    Key? key,
+    this.text,
+    this.textStyle,
+    this.height = 22,
+    this.width = 22,
+    this.borderRadius = 3,
+    this.bgColor,
+    this.textColor,
+    this.padding,
+  }) : super(key: key);
+
+  final String? text;
+  final TextStyle? textStyle;
+  final double? height;
+  final double? width;
+  final double borderRadius;
+  final Color? bgColor;
+  final Color? textColor;
+  final EdgeInsetsGeometry? padding;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: width,
+      height: height,
+      padding: padding ?? EdgeInsets.zero,
+      decoration: BoxDecoration(
+        color: bgColor ?? AppColor.secondaryToneFourEEF1FA,
+        borderRadius: BorderRadius.circular(borderRadius),
+      ),
+      child: Center(
+        child: Text(
+          text != null ? (text!.isNotEmpty ? text!.substring(0, 1) : '') : '',
+          style: AppTextStyle.headline6(
+            color: textColor ?? AppColor.secondaryFour877DF8,
+          ),
+        ),
+      ),
+    );
+  }
+}

RIZVY91221/wordpress_assignment

lib/core/uttils/functions.dart
@@ -1,0 +1,230 @@
+import 'dart:convert';
+import 'dart:io';
+
+import 'package:bs_assignment/core/theme/colors.dart';
+import 'package:bs_assignment/core/theme/text.dart';
+import 'package:bs_assignment/core/utils/extensions.dart';
+import 'package:bs_assignment/core/uttils/icons.dart';
+import 'package:bs_assignment/core/widget/global/text/wid_icon_text.dart';
+import 'package:bs_assignment/environment/build_config.dart';
+import 'package:bs_assignment/generated/assets.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:get/get.dart';
+
+/* Debug Print */
+appPrint(dynamic data) => BuildConfig.instance.envVariables.shouldCollectCrashLog ? debugPrint('$data') : '';
+
+class FN extends Deprecated {
+  FN(super.message);
+
+  /* Debug Print */
+  @Deprecated('Use "appPrint()" instead')
+  static print(dynamic data) => appPrint(data);
+
+  /* Combine Error Messages */
+  static String getErrorMessageCombined(Map<String, dynamic> errors) {
+    int index = 0;
+    String message = '';
+
+    errors.forEach((key, value) {
+      index++;
+      message += value[0];
+      if (errors.length != index) {
+        message += '\n';
+      }
+    });
+    return message;
+  }
+
+  /* ***
+   * Convert Base 64 Images into Viewable Widget
+   * **/
+
+  static Widget imageWidgetFromBase64(
+    String stringBase64, {
+    String filterOutBefore = '',
+    String filterOutAfter = '',
+    double? height,
+    double? width,
+    BoxFit? fit,
+    Color? color,
+    Color? textColor,
+    Widget? onErrorWidget,
+    Widget? onErrorPlaceholderWidget,
+    AlignmentGeometry? alignment,
+    BlendMode? colorBlendMode,
+    bool onErrorDefaultPlaceholder = false,
+    bool isText = false,
+    double borderRadius = 3,
+    Function(Object, StackTrace?)? onError,
+  }) {
+    Container box({Widget? child}) => Container(
+          height: height,
+          width: width,
+          color: color,
+          decoration: BoxDecoration(
+            color: color,
+            borderRadius: BorderRadius.circular(borderRadius),
+          ),
+          child: child,
+        );
+
+    Image noImage = Image.asset(Assets.imagesNoImage);
+
+    /// Check Blank
+    if (stringBase64.isEmpty || stringBase64 == 'null') return box(child: onErrorWidget ?? noImage);
+
+    /// When Widget is Text
+    if (isText) {
+      appPrint('⇢ ⇢ ⇢ Text:  "String"');
+      return WidgetIconWithLetter(
+        text: stringBase64,
+        height: height,
+        width: width,
+        bgColor: color,
+        textColor: textColor,
+        borderRadius: borderRadius,
+      );
+    }
+
+    /// When None of Above
+    Uint8List? bytes;
+
+    try {
+      bytes = getFromBase64(
+        stringBase64,
+        filterOutBefore: filterOutBefore,
+        filterOutAfter: filterOutAfter,
+      );
+    } catch (_) {}
+
+    Image? image;
+
+    Widget? svgImage;
+
+    String initializer({int char = 4}) => stringBase64.substring(0, char);
+
+    String extension() {
+      String lastTen = stringBase64.substring(stringBase64.length - 10, stringBase64.length);
+      String ext = 'base64';
+      try {
+        ext = lastTen.split('.')[1];
+      } catch (_) {}
+      return ext;
+    }
+
+    Widget errorWidget() => (onErrorDefaultPlaceholder ? noImage : (onErrorPlaceholderWidget ?? const SizedBox.shrink()));
+
+    appPrint('⇢ ⇢ ⇢ Image: "${initializer(char: 6)}◦◦◦.${extension()}"');
+
+    if (extension() == 'svg') {
+      if (initializer() == 'http') {
+        svgImage = appSVGNetworkComplex(
+          stringBase64,
+          color: color,
+          width: width,
+          height: height,
+        );
+      } else {
+        svgImage = appSVG(
+          stringBase64,
+          color: color,
+          width: width,
+          height: height,
+        );
+      }
+    } else {
+      if (initializer() == 'http') {
+        image = Image.network(
+          stringBase64,
+          fit: fit,
+          errorBuilder: (_, obj, trace) {
+            onError?.call(obj, trace);
+            return errorWidget();
+          },
+        );
+      } else if (initializer(char: 6) == 'assets') {
+        image = Image.asset(
+          stringBase64,
+          fit: fit,
+          errorBuilder: (_, obj, trace) {
+            onError?.call(obj, trace);
+            return errorWidget();
+          },
+        );
+      } else {
+        image = Image.file(
+          File(stringBase64),
+          fit: fit,
+          errorBuilder: (_, obj, trace) {
+            onError?.call(obj, trace);
+            return errorWidget();
+          },
+        );
+      }
+    }
+
+    return bytes != null
+        ? Image.memory(
+            bytes,
+            height: height,
+            width: width,
+            fit: fit,
+            color: color,
+            alignment: alignment ?? Alignment.center,
+            colorBlendMode: colorBlendMode,
+          )
+        : box(child: onErrorWidget ?? image ?? svgImage);
+  }
+
+  /* ***
+   * Decode Base 64 Images as "Uint8List"
+   * Have to view in "Image.memory()" Widget
+   * **/
+
+  static Uint8List getFromBase64(
+    String stringBase64, {
+    String filterOutBefore = '',
+    String filterOutAfter = '',
+  }) {
+    late Uint8List bytes;
+    if (filterOutBefore.isNotEmpty || filterOutAfter.isNotEmpty) {
+      bytes = base64.decode(stringBase64.toSubstring(fromStart: filterOutBefore.length, fromEnd: filterOutAfter.length));
+    } else {
+      bytes = base64.decode(stringBase64.split(';base64,').last);
+    }
+    return bytes;
+  }
+
+  static String getStatus(dynamic item) {
+    if (item != null) {
+      return item.map((e) => e is String ? e : e["name"]).toList().join(", ").toString();
+    } else {
+      return '-';
+    }
+  }
+
+  static copyText(String text) async {
+    ClipboardData? data = await Clipboard.getData('text/plain');
+    var copiedText = data?.text;
+    if (copiedText?.trim() == text.trim()) {
+      return;
+    } else {
+      Clipboard.setData(ClipboardData(text: text)).then((_) {
+        Get.rawSnackbar(
+            messageText: AppText.bodyExtraSmall("Copied", textAlign: TextAlign.center, color: AppColor.dark202125),
+            padding: const EdgeInsets.all(5),
+            backgroundColor: Colors.grey.withOpacity(0.2),
+            animationDuration: const Duration(milliseconds: 500),
+            duration: const Duration(milliseconds: 1000));
+      });
+    }
+  }
+
+  static dynamic getCopyText() async {
+    ClipboardData? data = await Clipboard.getData('text/plain');
+    var text = data?.text;
+    return text;
+  }
+}

lib/core/widget/global/card/card_item.dart
@@ -1,0 +1,84 @@
+import 'package:bs_assignment/core/theme/colors.dart';
+import 'package:bs_assignment/core/theme/text.dart';
+import 'package:flutter/material.dart';
+
+class ItemCard extends StatefulWidget {
+  const ItemCard(
+      {super.key,
+      required this.detailsKey,
+      this.detailsValue = '',
+      this.titleColor,
+      this.isExpand = true,
+      this.valueColor,
+      this.valueChild});
+
+  final String detailsKey;
+  final String detailsValue;
+  final Color? titleColor;
+  final Color? valueColor;
+  final bool? isExpand;
+  final Widget? valueChild;
+
+  @override
+  State<ItemCard> createState() => _ItemCardState();
+}
+
+class _ItemCardState extends State<ItemCard> {
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisSize: MainAxisSize.max,
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        if (widget.detailsKey != '') AppText.bodyVerySmall(widget.detailsKey, color: widget.titleColor ?? AppColor.darkLight4D4D50),
+        if (widget.detailsValue != '')
+          !widget.isExpand!
+              ? widget.detailsValue.contains("Canceled")
+                  ? CanceledValueChecker(
+                      value: widget.detailsValue,
+                    )
+                  : AppText.bodyVerySmall(widget.detailsValue, color: widget.valueColor ?? AppColor.dark202125)
+              : Expanded(
+                  child: widget.detailsValue.contains("Canceled")
+                      ? CanceledValueChecker(
+                          value: widget.detailsValue,
+                        )
+                      : AppText.bodyVerySmall(widget.detailsValue, color: widget.valueColor ?? AppColor.dark202125),
+                ),
+        if (widget.valueChild != null)
+          Padding(
+            padding: const EdgeInsets.only(left: 3.0),
+            child: widget.valueChild!,
+          )
+      ],
+    );
+  }
+}
+
+class CanceledValueChecker extends StatelessWidget {
+  const CanceledValueChecker({Key? key, required this.value}) : super(key: key);
+
+  final String value;
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+        children: value
+            .split(',')
+            .toList()
+            .asMap()
+            .map((index, e) => MapEntry(
+                index,
+                Row(
+                  children: [
+                    AppText.bodyVerySmall(e.contains("Canceled") ? "Cancelled" : e,
+                        color: e.contains("Canceled") ? AppColor.errorFE6C44 : AppColor.dark202125),
+                    index == value.split(',').toList().length - 1
+                        ? const SizedBox.shrink()
+                        : AppText.bodyVerySmall(',', color: AppColor.dark202125)
+                  ],
+                )))
+            .values
+            .toList());
+  }
+}

lib/core/widget/global/card/card_title.dart
@@ -1,0 +1,106 @@
+import 'package:bs_assignment/core/theme/colors.dart';
+import 'package:bs_assignment/core/theme/text.dart';
+import 'package:bs_assignment/core/uttils/functions.dart';
+import 'package:bs_assignment/core/uttils/icons.dart';
+import 'package:bs_assignment/generated/assets.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+enum IconDirection { left, right }
+
+// ignore: must_be_immutable
+class CardTitleWithIcon extends StatelessWidget {
+  CardTitleWithIcon({
+    Key? key,
+    this.storeIcon,
+    required this.title,
+    this.textAsTitle = true,
+    this.showCopyButton = true,
+    this.iconDirection = IconDirection.left,
+    this.mainAxisSize = MainAxisSize.max,
+  }) : super(key: key);
+
+  final String? storeIcon;
+  final String title;
+  final bool textAsTitle;
+  final bool showCopyButton;
+  final IconDirection iconDirection;
+  final MainAxisSize mainAxisSize;
+
+  var copiedDone = false.obs;
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisSize: mainAxisSize,
+      children: [
+        iconWidget(iconDirection == IconDirection.left),
+        orderIdText(),
+        iconWidget(iconDirection == IconDirection.right),
+        if (showCopyButton) copyButtonWidget(),
+      ],
+    );
+  }
+
+  /// Icon Widget
+  Widget iconWidget(bool show) => (show && storeIcon != null)
+      ? Padding(
+          padding: const EdgeInsets.only(right: 5),
+          child: ClipRRect(
+            borderRadius: const BorderRadius.all(Radius.circular(4)),
+            child: FN.imageWidgetFromBase64(
+              storeIcon!.isNotEmpty ? storeIcon! : title.characters.elementAt(1),
+              fit: BoxFit.cover,
+              isText: storeIcon!.isEmpty ? true : false,
+              width: storeIcon!.isNotEmpty ? 18 : 22,
+              height: storeIcon!.isNotEmpty ? 18 : 22,
+            ),
+          ))
+      : const SizedBox.shrink();
+
+  /// Order IdD Text Widget
+  Padding orderIdText() {
+    return Padding(
+      padding: const EdgeInsets.only(right: 5),
+      child: textAsTitle ? AppText.bodySmall(title, color: AppColor.dark202125) : AppText.bodyVerySmall(title, color: AppColor.dark202125),
+    );
+  }
+
+  /// Order No Widget
+  Obx copyButtonWidget() {
+    return Obx(
+      () => InkWell(
+        splashColor: Colors.transparent,
+        highlightColor: Colors.transparent,
+        onTap: () async {
+          await FN.copyText(title);
+          copiedDone.value = true;
+          Future.delayed(const Duration(seconds: 2), () {
+            copiedDone.value = false;
+          });
+        },
+        child: SizedBox(
+          height: 15,
+          width: 40,
+          child: copiedDone.value
+              ? Align(
+                  alignment: Alignment.centerLeft,
+                  child: Icon(
+                    Icons.check,
+                    color: AppColor.darkLightest6C7576,
+                    size: 14,
+                  ),
+                )
+              : Align(
+                  alignment: Alignment.centerLeft,
+                  child: appSVG(
+                    Assets.svgCopyIcon,
+                    color: AppColor.darkLightest6C7576,
+                    width: 14,
+                    height: 18,
+                  ),
+                ),
+        ),
+      ),
+    );
+  }
+}

lib/core/widget/global/text/wid_icon_text.dart
@@ -1,0 +1,47 @@
+import 'package:bs_assignment/core/theme/colors.dart';
+import 'package:bs_assignment/core/theme/text.dart';
+import 'package:flutter/material.dart';
+
+class WidgetIconWithLetter extends StatelessWidget {
+  const WidgetIconWithLetter({
+    Key? key,
+    this.text,
+    this.textStyle,
+    this.height = 22,
+    this.width = 22,
+    this.borderRadius = 3,
+    this.bgColor,
+    this.textColor,
+    this.padding,
+  }) : super(key: key);
+
+  final String? text;
+  final TextStyle? textStyle;
+  final double? height;
+  final double? width;
+  final double borderRadius;
+  final Color? bgColor;
+  final Color? textColor;
+  final EdgeInsetsGeometry? padding;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: width,
+      height: height,
+      padding: padding ?? EdgeInsets.zero,
+      decoration: BoxDecoration(
+        color: bgColor ?? AppColor.secondaryToneFourEEF1FA,
+        borderRadius: BorderRadius.circular(borderRadius),
+      ),
+      child: Center(
+        child: Text(
+          text != null ? (text!.isNotEmpty ? text!.substring(0, 1) : '') : '',
+          style: AppTextStyle.headline6(
+            color: textColor ?? AppColor.secondaryFour877DF8,
+          ),
+        ),
+      ),
+    );
+  }
+}